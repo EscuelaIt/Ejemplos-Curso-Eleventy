9ee9da0dd0009cf7e87c5dad22d2df321c75fb06
midesweb <malvarez@desarrolloweb.com>
2025-10-01 18:49:58 +0200

    primeros pasos

---
+++ about.md
@@ -0,0 +1,11 @@
+---
+layout: layout.html
+title: Acerca de Miguel
+author: Miguel Angel Alvarez
+---
+
+# Acerca de
+
+Esta es la página de "Acerca de" que estamos creando en el curso de Eleventy en EscuelaIT.
+
+Esto es un cambio. Y a la vez estoy viendo el contenido que cambio solamente con guardar.
---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let boton = document.getElementById('miboton');
+  boton.addEventListener('click', function() {
+    boton.textContent = 'has hecho click';
+  });
+})
+
+$(document).ready(function() {
+  $("#menu").on("click", function() {
+    $("#modal").fadeIn(300).css("display", "flex");
+  });
+
+  $("#cerrar").on("click", function() {
+    $("#modal").fadeOut(300);
+  });
+});
