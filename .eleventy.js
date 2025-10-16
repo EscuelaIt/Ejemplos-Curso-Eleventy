22c6321600ba3aa8ad08da3f36900c5ad220a630
midesweb <malvarez@desarrolloweb.com>
2025-10-16 10:21:11 +0200

    incorporamos tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
 import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("docs/assets/css");
     eleventyConfig.addPassthroughCopy("docs/assets/js");
     eleventyConfig.addPassthroughCopy("docs/assets/images");
     
-    eleventyConfig.addPlugin(EleventyVitePlugin);
+    eleventyConfig.addPlugin(EleventyVitePlugin, {
+        viteOptions: {
+            plugins: [
+                tailwindcss(),
+            ],
+        }
+    });
     
     return {
         dir: {
